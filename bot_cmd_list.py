ddfb1b6827d9a48679fd4e650879da6bf1f302c6
nikryt <nikryt@gmail.com>
2025-05-17 22:27:01 +0300

    Test

--- bot_cmd_list.py
+++ bot_cmd_list.py
@@ -2,6 +2,6 @@ from     aiogram.types import BotCommand
 
 private = [
     BotCommand(command='start', description='Запуск бота'),
-    BotCommand(command='register', description='Добавить селфи'),
+    BotCommand(command='register', description='Добавить еще селфи'),
     BotCommand(command='help', description='Помощь'),
 ]
